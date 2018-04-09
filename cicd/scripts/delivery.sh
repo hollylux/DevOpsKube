e9d1425fb7e3046adf4362448f6cb55b3cd1c27a
hollylux <hollyluxl@gmail.com>
2018-04-09 15:13:52 -0400

    put service deployment into one file

--- cicd/scripts/delivery.sh
+++ cicd/scripts/delivery.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f cicd/kubefiles/deployments/gosayhello.yaml
-kubectl apply -f cicd/kubefiles/services/gosayhello.yaml
+kubectl apply -f cicd/kubefiles/gosayhello.yaml
 
 exit 0
