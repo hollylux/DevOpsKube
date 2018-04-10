c59750f5243f2c5a5dc17f822a8bd6df24c3d4f0
hollylux <hollyluxl@gmail.com>
2018-04-10 08:11:10 -0400

    changing docker build context path

--- cicd/scripts/build.sh
+++ cicd/scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go build --tags netgo --ldflags '-extldflags "-lm -lstdc++ -static"' -o bin/sayhello src/com/blue/sayhello.go
+go build --tags netgo --ldflags '-extldflags "-lm -lstdc++ -static"' -o ./bin/sayhello ./src/com/blue/sayhello.go
 #docker image rm -f brucelu/gosayhello:latest docker.io/brucelu/gosayhello
-docker build -t brucelu/gosayhello:latest -f cicd/Dockerfile .
+docker build -t brucelu/gosayhello:latest -f cicd/Dockerfile ./bin
 docker push brucelu/gosayhello
 
 exit 0
