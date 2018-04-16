bf516288302095e83e2760726fe99b611d993afe
hollylux <hollyluxl@gmail.com>
2018-04-16 14:57:15 -0400

    v11

--- src/com/blue/sayhello.go
+++ src/com/blue/sayhello.go
@@ -22,7 +22,7 @@ func sayHello(res http.ResponseWriter, req *http.Request) {
 
 	msg := req.URL.Path
 	msg = strings.TrimPrefix(msg, "/")
-	msg = cur + " : " + msg + " from " + hostname + ". By Bruce using Go lang. v10"
+	msg = cur + " : " + msg + " from " + hostname + ". By Bruce using Go lang. v11"
 
 	fmt.Println(msg)
 
